ee03a42ff46cce423ff16f2a91617976c4c7f4ea
igABHINAV <104921944+igABHINAV@users.noreply.github.com>
2024-02-14 01:10:09 +0530

    Update Googleauth.js

--- server/config/Googleauth.js
+++ server/config/Googleauth.js
@@ -1,7 +1,7 @@
 const passport = require("passport");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require("../models/User");
-
+require("dotenv").config();
 // Define the GoogleStrategy
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID, // Use process.env to get environment variables
