64014686c8e72c21a637d6af5f4a26c83cdeea1f
Abhay Singh <singhxabhay@gmail.com>
2024-10-30 14:03:31 +0530

    fixed loader not appearing issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import LoaderWrapper from "../components/LoaderWrapper"; // Adjust the path if needed
 import Footer from "../components/ui/footer/footer"; // Import the Footer component
 import { Suspense } from "react";
+import Loader from "../components/loader"; // Import the Loader component
 
 const inter = Inter({ subsets: ["latin"] });
 import type { Metadata } from "next";
@@ -20,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<Loader />}>
           <LoaderWrapper>{children}</LoaderWrapper>
         </Suspense>
         <Footer /> {/* Add the Footer component here */}
