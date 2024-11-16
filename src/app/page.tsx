74832a0792467dbf89d270aa3bc4ca04a363690a
Shuvam Patra <91253815+BlackEyes09@users.noreply.github.com>
2024-11-16 20:43:31 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -112,30 +112,30 @@ export default function Home() {
   );
 }
 
-export function TailwindcssButtons() {
-  return (
-    <div>
-      {buttons.map((button, idx) => (
-        <ButtonsCard key={idx}>
-          {button.component}
-        </ButtonsCard>
-      ))}
-    </div>
-  );
-}
+// export function TailwindcssButtons() {
+//   return (
+//     <div>
+//       {buttons.map((button, idx) => (
+//         <ButtonsCard key={idx}>
+//           {button.component}
+//         </ButtonsCard>
+//       ))}
+//     </div>
+//   );
+// }
 
-export const buttons = [
-  {
-    component: (
-      <button
-        className="shadow-[0_0_0_3px_teal-400_inset] px-6 py-2 bg-transparent border border-teal-400 dark:border-teal-400 dark:text-teal-400 text-teal-400 font-bold transform hover:-translate-y-1 transition duration-400 hover:border-white hover:text-white"
-        onClick={() => window.location.href = '/join'}
-      >
-        Be A Member
-      </button>
-    ),
-  },
-];
+// export const buttons = [
+//   {
+//     component: (
+//       <button
+//         className="shadow-[0_0_0_3px_teal-400_inset] px-6 py-2 bg-transparent border border-teal-400 dark:border-teal-400 dark:text-teal-400 text-teal-400 font-bold transform hover:-translate-y-1 transition duration-400 hover:border-white hover:text-white"
+//         onClick={() => window.location.href = '/join'}
+//       >
+//         Be A Member
+//       </button>
+//     ),
+//   },
+// ];
 
 export function BentoGridDemo() {
   return (
